a98b4ce13ed804c2dbe91cc260b1a4276ed19e2d
codegeek03 <shamikbhattacharjee03@kgpian.iitkgp.ac.in>
2023-07-04 02:17:15 +0530

    m

--- script.js
+++ script.js
@@ -9,10 +9,16 @@ but.addEventListener("click", function() {
   });
 
 var res=document.getElementById("cv")
-res.addEventListener("click",function(){
-    var cvUrl = "https://github.com/codegeek03/python-codes/blob/main/Profile.pdf"; // Replace with the actual path to your CV file
-    var link = document.createElement("a");
-    link.href = cvUrl;
-    link.download = "cv.pdf"; // Specify the desired name for the downloaded file
-    link.click();
-})
+const fileUrl = 'https://github.com/codegeek03/python-codes/blob/main/Profile.pdf';
+const fileName = 'myFile.pdf';
+downloadFile(fileUrl, fileName);
+function downloadFile(url, filename) {
+    const anchorElement = document.createElement('a');
+    anchorElement.href = url;
+      anchorElement.download = filename;
+    anchorElement.style.display = 'none';
+    document.body.appendChild(anchorElement);
+    anchorElement.click();
+    document.body.removeChild(anchorElement);
+  }
+  
